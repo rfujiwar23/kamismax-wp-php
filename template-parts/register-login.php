24b042727f748511a241feb15257c628c38f434e
Ryo Fujiwara <jjcproject@icloud.com>
2023-03-28 19:08:54 +0900

    changes to the button on the top page

--- template-parts/register-login.php
+++ template-parts/register-login.php
@@ -3,11 +3,6 @@
         <li class="login"><a href="https://kamismax.kamisma.com/login">ログイン</a></li>
         <li class="register"><a href="https://kamismax.kamisma.com/login">会員登録</a></li>
     </ul>
-    <style>
-        .bg {
-            background: #fff;
-        }
-    </style>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
